f8b4304271b9c8ce2516c89194236a34fd3ab1e2
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-03-13 12:44:06 -0300

    Weighted probability calculation setup

---
+++ src/main/java/net/mega2223/lwjgltest/aguaengine3d/mathematics/MathUtils.java
@@ -0,0 +1,18 @@
+package net.mega2223.lwjgltest.aguaengine3d.mathematics;
+
+import java.util.Random;
+
+public class MathUtils {
+    public static Object doWeightedSelection(Object[] objects, float[] weights){
+        if(objects.length!= weights.length){throw new UnsupportedOperationException("Objects or weights not set up correctly");}
+        float totalWeightLenght = 0;
+        for(float act : weights){totalWeightLenght+=act;}
+        float randomValue = new Random().nextFloat() * totalWeightLenght;
+        float currentPos = 0;
+        for(int i = 0; i < objects.length; i++){
+            if(randomValue >= currentPos && randomValue < currentPos+weights[i]){return objects[i];}
+            currentPos+=weights[i];
+        }
+        return null;
+    }
+}
---
+++ src/test/java/ObjectWeightSelectionTest.java
@@ -0,0 +1,25 @@
+import net.mega2223.lwjgltest.aguaengine3d.mathematics.MathUtils;
+
+public class ObjectWeightSelectionTest {
+
+    public static void main(String[] args) {
+        String[] prob = {"50","25","10","15"};
+        float[] probs = {.5f,.25f,.1f,.15f};
+
+        int count[] = new int[4];
+
+        for (int i = 0; i < 10000; i++) {
+            Object selected = MathUtils.doWeightedSelection(prob, probs);
+            if(selected.equals("50")){count[0]++;}
+            if(selected.equals("25")){count[1]++;}
+            if(selected.equals("10")){count[2]++;}
+            if(selected.equals("15")){count[3]++;}
+            System.out.println(selected);
+        }
+        System.out.println("Stats:\n");
+        for (int i = 0; i < prob.length; i++) {
+            System.out.println(probs[i] + ": " + count[i]);
+        }
+    }
+
+}
