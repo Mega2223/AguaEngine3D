9c31ce1c5c167f33c4fac46b79c13178e2c75cf3
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2025-09-04 18:43:57 -0300

    Função doWeightedSelection agora é genérica Ou seja vc não precisa castar após chamar a função :3

--- src/main/java/net/mega2223/aguaengine3d/mathematics/MathUtils.java
+++ src/main/java/net/mega2223/aguaengine3d/mathematics/MathUtils.java
@@ -7,12 +7,12 @@ public class MathUtils {
     public static Object doWeightedSelection(@SuppressWarnings("rawtypes") List objectList, float[] weights){
         return doWeightedSelection(objectList.toArray(),weights);
     }
-    public static Object doWeightedSelection(Object[] objects, float[] weights){
+    public static <T> T doWeightedSelection(T[] objects, float[] weights){
         if(objects.length!= weights.length){throw new UnsupportedOperationException("Objects or weights not set up correctly");}
         if(objects.length == 1){return objects[0];}
-        float totalWeightLenght = 0;
-        for(float act : weights){totalWeightLenght+=act;}
-        float randomValue = new Random().nextFloat() * totalWeightLenght;
+        float weightSum = 0;
+        for(float act : weights){weightSum+=act;}
+        float randomValue = new Random().nextFloat() * weightSum;
         float currentPos = 0;
         for(int i = 0; i < objects.length; i++){
             if(randomValue >= currentPos && randomValue < currentPos+weights[i]){return objects[i];}
@@ -36,11 +36,11 @@ public class MathUtils {
         return ret;
     }
 
-    public static void predictNextPos(float[] initialVector, float vectorSpeed, float directionRadians){
-        predictNextPos(initialVector,0,1,vectorSpeed,directionRadians);
+    public static void predict2D(float[] initialVector, float vectorSpeed, float directionRadians){
+        predict2D(initialVector,0,1,vectorSpeed,directionRadians);
     }
 
-    public static void predictNextPos(float[] initialVector, int xLocationInArray, int zLocationInArray, float vectorSpeed, float directionRadians){
+    public static void predict2D(float[] initialVector, int xLocationInArray, int zLocationInArray, float vectorSpeed, float directionRadians){
         float x = initialVector[xLocationInArray] , z = initialVector[zLocationInArray];
         float s = (float) Math.sin(directionRadians);
         float c = (float) Math.cos(directionRadians);
@@ -68,7 +68,6 @@ public class MathUtils {
         }
 
         return smallestDis;
-
     }
 
     public static void scaleAllElements(float[] array, float factor){
@@ -77,7 +76,7 @@ public class MathUtils {
         }
     }
 
-    public static float getHighestNInDataset(float[] data){
+    public static float highestValueInArray(float[] data){
         float f = data[0];
         for (int i = 1; i < data.length; i++) {
             f = Math.max(f,data[i]);
