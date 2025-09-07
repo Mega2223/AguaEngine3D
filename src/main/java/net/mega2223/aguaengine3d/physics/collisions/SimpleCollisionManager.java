b189186012ee811622d50c9462f87c69f25e3ce0
Mega2223 <mega2223gamer@gmail.com>
2025-09-07 17:10:05 -0300

    h

--- src/main/java/net/mega2223/aguaengine3d/physics/collisions/SimpleCollisionManager.java
+++ src/main/java/net/mega2223/aguaengine3d/physics/collisions/SimpleCollisionManager.java
@@ -2,8 +2,6 @@ package net.mega2223.aguaengine3d.physics.collisions;
 
 import net.mega2223.aguaengine3d.physics.PhysicsContext;
 import net.mega2223.aguaengine3d.physics.PhysicsObject;
-import net.mega2223.aguaengine3d.physics.objects.Particle;
-import net.mega2223.aguaengine3d.physics.collisions.CollisionMath;
 
 import java.util.List;
 
@@ -14,17 +12,17 @@ public class SimpleCollisionManager extends CollisionManager{
 
     @Override
     public void manageCollisions(float deltaT) {
-        System.out.println("g");
+        //System.out.println("g");
         List<PhysicsObject> objects = physContext.getObjects(); // fixme maybe too resource intensive
         for(PhysicsObject o1 : objects){
             o1 = o1.getActor();
             if(!(o1 instanceof Collideable)){continue;}
-            System.out.println("h");
+            //System.out.println("h");
             for(PhysicsObject o2 : objects){
                 o2 = o2.getActor();
                 if(o1 == o2 || !(o2 instanceof Collideable)){continue;}
                 if (((Collideable) o1).collidesWith((Collideable) o2)){
-			System.out.println("COLLISION RAAARRRHHHH");
+			//System.out.println("COLLISION RAAARRRHHHH");
 			CollisionMath.solveCollision(o1, o2, 1.0F);
                 }
             }
